8fd6c756eac7a9a67cfdd79b96afc837c76b6323
kwaniek <kwaniek@lol.nope>
2017-03-28 23:45:35 +0200

    travis initial setting

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('city-communication-app App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Init content');
   });
 });
